9387d5ed724c417ea07ce2866e359e84297b0dcf
MaximeGuiraud <maxime.guiraud28@gmail.com>
2016-04-26 10:40:31 +0200

    Mise en place User :
    Mise en place des roles d'utilisateur.
    Création de l'entité inscrit.

---
+++ src/EchangeoBundle/Entity/inscrit.php
@@ -0,0 +1,55 @@
+<?php
+// src/AppBundle/Entity/User.php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class Inscrit extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="prenom", type="string", length=255)
+     */
+    private $prenom;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_de_naissance", type="date")
+     */
+    private $dateNaissance;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="adresse", type="string", length=255)
+     */
+    private $adresse;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
