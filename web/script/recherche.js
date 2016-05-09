3d2e00f4d36673884c4a896a9bb4adab5a8759de
MaximeGuiraud <maxime.guiraud28@gmail.com>
2016-05-09 10:32:26 +0200

    Création fonction DisplaySousCatégorie :
    creation de la fonction dans le fichier rechercheScript.html.twig.
    utilisation d'un fichier twig pour permettre la création d'url avec le générateur de twig

--- web/script/recherche.js
+++ web/script/recherche.js
@@ -2,7 +2,11 @@
  * Ensembles des fonction utile à la recherche de services
  * par Guiraud Maxime
  * =======================================================*/
-function displaySousCategorie() {
-	console.log("display sous categorie");
+function displaySousCategorie(event) {
+	console.log("display sous categorie | numéro catégorie choisie :");
+	console.log(this.options[this.selectedIndex].value);
+	console.log("url :");
+	console.log(url);
+
 }
 
